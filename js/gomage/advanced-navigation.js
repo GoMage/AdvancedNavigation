5d404a6374fee40bf4f1d1dc74dca5b964573144
vetalkab <vb@gomage.com>
2013-11-16 18:55:46 +0200

    https://clients.gomage.com/issues/5784

--- js/gomage/advanced-navigation.js
+++ js/gomage/advanced-navigation.js
@@ -1317,17 +1317,22 @@ GomageNavigationClass = Class.create({
 				
 				this.navigationOpenFilters[request_var+'_is_open'] = true;
 
-                $(id + '-title').removeClassName('filter-content-hide');
-                $(id + '-title').addClassName('filter-content-show');
-				
+                if ( $(id + '-title') )
+                {
+                    $(id + '-title').removeClassName('filter-content-hide');
+                    $(id + '-title').addClassName('filter-content-show');
+                }
 			}else{
 				
 				$(id).style.display = 'none' ;
 				
 				this.navigationOpenFilters[request_var+'_is_open'] = false;
 
-                $(id + '-title').removeClassName('filter-content-show');
-                $(id + '-title').addClassName('filter-content-hide');
+                if ( $(id + '-title') )
+                {
+                    $(id + '-title').removeClassName('filter-content-show');
+                    $(id + '-title').addClassName('filter-content-hide');
+                }
 			}
 		}
 		
